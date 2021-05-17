ed16a89d3dae8c0b424b4212c1e60aa487f0390c
Quasimodo <47749932+Midubo@users.noreply.github.com>
2021-05-17 14:16:18 +0300

    Value Converter Basics

---
+++ ValueConverterBasics/ValueConverterBasics/BooleanToIntConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ValueConverterBasics
+{
+    class BooleanToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? 5 : 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToInt32(value) < 15 ? false : true;
+        }
+    }
+}
---
+++ ValueConverterBasics/ValueConverterBasics/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace ValueConverterBasics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool LicenseExpired { get; set; } = false;
+
+        public bool HasThickBorder { get; set; } = true;
+
+        public bool BoolProperty { get; set; }
+
+        public int Grade { get; set; } = 50;
+
+        public bool ShowAds { get; set; } = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
